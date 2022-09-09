ddb9696398a20fa3fc078129cf70304b0f32cc93
Ali Mahdiyar <alimahdiyar77@gmail.com>
2022-09-10 02:22:24 +0430

    fix: export isTheSameAddress

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.MulticallAbiHandler = exports.default = exports.MetamocksContext = exports.AbiHandler = void 0;
+exports.isTheSameAddress = exports.MulticallAbiHandler = exports.default = exports.MetamocksContext = exports.AbiHandler = void 0;
 var abihandler_1 = require("./abihandler");
 Object.defineProperty(exports, "AbiHandler", { enumerable: true, get: function () { return __importDefault(abihandler_1).default; } });
 var context_1 = require("./context");
@@ -17,3 +17,5 @@ var metamocks_1 = require("./metamocks");
 Object.defineProperty(exports, "default", { enumerable: true, get: function () { return __importDefault(metamocks_1).default; } });
 var multicall_1 = require("./abihandlers/multicall");
 Object.defineProperty(exports, "MulticallAbiHandler", { enumerable: true, get: function () { return __importDefault(multicall_1).default; } });
+var utils_1 = require("./utils");
+Object.defineProperty(exports, "isTheSameAddress", { enumerable: true, get: function () { return utils_1.isTheSameAddress; } });
--- types/index.d.ts
+++ types/index.d.ts
@@ -2,3 +2,4 @@ export { default as AbiHandler } from "./abihandler";
 export { default as MetamocksContext } from "./context";
 export { default } from "./metamocks";
 export { default as MulticallAbiHandler } from "./abihandlers/multicall";
+export { isTheSameAddress } from "./utils";
