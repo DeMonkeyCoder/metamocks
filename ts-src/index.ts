30b6ae156cdb1219af3edcf9f3006c6f6d58607e
Ali Mahdiyar <alimahdiyar77@gmail.com>
2022-09-10 02:20:39 +0430

    refactor: export isTheSameAddress

--- ts-src/index.ts
+++ ts-src/index.ts
@@ -8,3 +8,4 @@ export { default as AbiHandler } from "./abihandler";
 export { default as MetamocksContext } from "./context";
 export { default } from "./metamocks";
 export { default as MulticallAbiHandler } from "./abihandlers/multicall";
+export { isTheSameAddress } from "./utils";
